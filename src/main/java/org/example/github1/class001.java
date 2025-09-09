41afc5e20648dce4505855343acd02d30faf7c13
YJaafar34 <yasirjaafar2022@gmail.com>
2025-09-09 04:47:12 -0400

    person2commit

--- src/main/java/org/example/github1/class001.java
+++ src/main/java/org/example/github1/class001.java
@@ -2,7 +2,7 @@ package org.example.github1;
 
 public class class001 {
     public static void main(String[] args) {
-        System.out.println("hello World");
+        System.out.println("hello World version 6 by person 6");
 
     }
 
